d4401cce127de3f9d297b4760899c6dfc8d78455
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-10-03 21:58:23 -0300

    att

--- src/controllers/adressController.js
+++ src/controllers/adressController.js
@@ -1,9 +1,35 @@
-
+const AdressModel = require('../models/Adress');
+const jwt = require('jsonwebtoken');
 
 
 class adressController {
-
+    static async userAdress(req, res){
+        const {id} = req.params;
+        try {
+            const findAdress = await AdressModel.findOnde({userId: id})
+                .populate({path: 'userId', selected: 'name email admin createdDate'})
+                .exec();
+            res.status(200).send(findAdress);
+        } catch (error) {
+            res.status(404).send('Não foi possível buscar esse usuário!');
+        }
+    }
+    static async registerAdress (req, res){
+        const NewAdress = new AdressModel({
+            logradouro: req.body.logradouro,
+            complemento1: req.body.complemento1,
+            complemento2: req.body?.complemento2,
+            userId: req.body.userId,
+            cep: req.body.cep,
+        });
+        try {
+            const authorization  = jwt.verify(req.body.token, process.env.SECRET_TOKEN);
+            if(!authorization) return res.status(403).send('Fobidden');
+            await NewAdress.save();
+            res.status(201).send('Endereço cadastrado com sucesso!')
+        } catch (error) {
+            res.status(500).send('Houve um erro!')
+        }
+    }
 }
-
-
 module.exports = adressController;
--- src/models/Adress.js
+++ src/models/Adress.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 
 
+const AdressSchema = mongoose.Schema({
+    logradouro: {type: String, required: true},
+    bairro: {type: String, required:true},
+    complemento1: {type: String, required: true},
+    complemento2: {type: String},
+    cep:{type:String, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref:"user", required: true},    
+    createdDate: {type: Date, default: Date.now()}
+})
+
+
+const AdressModel = mongoose.model('adress', AdressSchema)
