59f2ae1da1061e5f11646c6ea10382c5f8d90dd6
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-05-31 18:30:41 -0300

    Feat add admin permision for add new products

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,12 +1,12 @@
-const productController = require('../controllers/productController');
-const express = require('express')
+const { authAdmin } = require("../auth/auth");
+const productController = require("../controllers/productController");
+const express = require("express");
 
 const productRouter = express.Router();
 
 productRouter
-    .get('/products/list/all', productController.listAllProducts)
-    .get('/products/list/:id', productController.listOneProduct)
-    .post('/products/new', productController.createProduct)
-
+  .get("/products/list/all", productController.listAllProducts)
+  .get("/products/list/:id", productController.listOneProduct)
+  .post("/products/new", authAdmin, productController.createProduct);
 
 module.exports = productRouter;
