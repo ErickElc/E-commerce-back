d9cf2920c0e58b558f89d369632dbc1155d1cb62
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-05-31 18:29:57 -0300

    fix handle routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,29 +1,19 @@
-const productRouter = require('./product.routes');
-const entregaRouter = require('./entrega.routes');
-const routerUser = require('./user.routes');
-const routerAuth = require('./auth.routes');
-const express = require('express')
+const productRouter = require("./product.routes");
+const entregaRouter = require("./entrega.routes");
+const routerUser = require("./user.routes");
+const routerAuth = require("./auth.routes");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-
-
-const routes = app =>{
-    app.use(
-        '/api',
-        express.urlencoded({ extended: false }),
-        routerUser,
-        productRouter,
-        entregaRouter
-    )
-    app.use(
-        '/auth/',
-        express.urlencoded({ extended: false }),
-        routerAuth
-    )
-}
-
+const routes = (app) => {
+  app.use(
+    "/api",
+    express.urlencoded({ extended: false }),
+    routerUser,
+    productRouter,
+    entregaRouter
+  );
+  app.use("/auth", express.urlencoded({ extended: false }), routerAuth);
+};
 
 module.exports = routes;
-
-
-
-
