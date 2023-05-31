1991728e072e9e09735218f3d57d36ddaf48f4f3
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-05-31 18:24:36 -0300

    Package.json & app.js

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const routes = require('./src/routes/index.routes');
-const db = require('./src/config/db');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const routes = require("./src/routes/index.routes");
+const db = require("./src/config/db");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8877;
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 routes(app);
 
+app.listen(PORT, () => {
+  console.log(`Server escutando na porta ${PORT}`);
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server escutando na porta ${PORT}`);
-})
+db.on("error", () => {
+  console.log("não foi possível se conectar com banco de dados");
+});
 
-db.on('error', ()=> {console.log('não foi possível se conectar com banco de dados')});
-
-db.once('open', ()=>{
-    console.log('Conexão com o banco de dados feito com sucesso!')
-})
+db.once("open", () => {
+  console.log("Conexão com o banco de dados feito com sucesso!");
+});
