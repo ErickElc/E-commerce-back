15ae171de4564871608a23434ab36fca19a85484
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-06-02 11:14:45 -0300

    feat: new attributes age, phone number e cpf

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String , required: true, unique: true},
-    password: {type: String, required:true},
-    admin: {type: Boolean, default: false},
-    createdDate: {type: Date, default: Date.now()}
+  name: { type: String, required: true },
+  cpf: {
+    type: String,
+    required: true,
+    unique: true,
+    default: "000.000.000-00",
+  },
+  age: { type: Number, required: true, default: 0 },
+  phoneNumber: { type: String, required: true, default: "(00) 00000-0000" },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: { type: Boolean, default: false },
+  createdDate: { type: Date, default: Date.now() },
 });
 
-
-
-const userModel = mongoose.model('user', userSchema);
-
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = userModel;
