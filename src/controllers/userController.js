13e3f6409a212ab06fe0df586367b04b7fd56073
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-06-02 11:14:18 -0300

    Feat: UpdateuserData and delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,33 @@
+const formatPublicData = require("../utils/data");
 const userModel = require("../models/User");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const salt = bcryptjs.genSaltSync(10);
+/*
+  USERS CONTROLLER
+  1. modificar dados do usuário FEITO
+  2. deletar usuário FEITO
+  5. Modificar os produtos 
+  6. criar categorias
+  7. modificar categorias
+  8. deletar categorias
+  9. deletar produtos
+  10. adicionar imagem aos produtos
+  11. adicionar imagem ao usuário
+  12. adicionar imagem a categoria
+  13. adicionar métodos de pagamento na plataforma
+  
+*/
 class userController {
   static async registerUser(req, res) {
     try {
       const findEmail = await userModel.findOne({ email: req.body.email });
       if (findEmail) return res.status(404).send("Email já utilizado");
-      const cryptpassword = bcryptjs.hashSync(req.body.password, salt);
+      const encryptedPassword = bcryptjs.hashSync(req.body.password, salt);
       const createUser = new userModel({
         name: req.body.name,
         email: req.body.email,
-        password: cryptpassword,
+        password: encryptedPassword,
       });
       await createUser.save();
       res.status(201).send("Usuário Criado Com sucesso");
@@ -19,6 +35,7 @@ class userController {
       res.status(500).send("Não foi possível criar usuário!" + error);
     }
   }
+
   static async loginUser(req, res) {
     try {
       const userSelected = await userModel.findOne({ email: req.body.email });
@@ -45,6 +62,7 @@ class userController {
       res.status(500).send("Não Foi possível fazer login " + error);
     }
   }
+
   static async listOneUser(req, res) {
     const id = req.params.id;
     if (!id)
@@ -67,28 +85,7 @@ class userController {
       };
       res.status(200).send(userData);
     } catch (error) {
-      res.status(400).send("Houve um erro: " + error);
-    }
-  }
-
-  static async listAllUsers(req, res) {
-    try {
-      const usersList = await userModel.find();
-      const userListFiltrated = [];
-
-      for (let i in usersList) {
-        userListFiltrated.push({
-          _id: usersList[i]._id,
-          name: usersList[i].name,
-          email: usersList[i].email,
-          admin: usersList[i].admin,
-          createdDate: usersList[i].createdDate,
-        });
-      }
-      console.log(userListFiltrated);
-      res.status(200).send(userListFiltrated);
-    } catch (error) {
-      res.status(400).send("Não foi possível buscar os usuários");
+      res.status(500).send("Houve um erro: " + error);
     }
   }
 
@@ -111,6 +108,56 @@ class userController {
           .status(403)
           .send("You are not authorized to access this page");
       res.status(400).send("Não foi possível buscar o usuário");
+    }
+  }
+
+  static async updateUserData(req, res) {
+    const id = req.params.id;
+    const { name, age, phoneNumber } = req.body;
+    if (!name && !age && !phoneNumber)
+      return res.status(400).send("Não foi possível atualizar os dados!");
+    try {
+      const userSelected = await userModel.findOneAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            name: name,
+            age: age,
+            phoneNumber: phoneNumber,
+          },
+        }
+      );
+
+      if (!userSelected)
+        return res
+          .status(400)
+          .send("Não foi possível atualizar os dados do usuário");
+
+      const userUpdated = await userModel.findOne({ _id: id });
+
+      if (!userUpdated)
+        return res
+          .status(500)
+          .send("Os dados foram atualizados, mas não foram retornados");
+
+      const userData = formatPublicData(userUpdated);
+
+      res.status(200).send(userData);
+    } catch (error) {
+      res.status(500).send("Não foi possível atualizar o usuário");
+    }
+  }
+  static async deleteUser(req, res) {
+    const id = req.params?.id;
+    if (!id) return res.status(400).send("Não foi possível deletar o usuário");
+    try {
+      const selectedUser = await userModel.findOneAndDelete({ _id: id });
+
+      if (!selectedUser) return res.status(404).send("Usuário não encontrado");
+
+      res.status(200).send("Usuário deletado com sucesso");
+    } catch (error) {
+      res.status(500).send("Não foi possível deletar o usuário");
     }
   }
 }
