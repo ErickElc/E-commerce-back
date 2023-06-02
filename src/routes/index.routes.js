d1a10029449a75c59fe466daa68929b4dd0a3637
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-06-02 11:15:38 -0300

    fix: names of routes and add the admin router

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,19 +1,20 @@
 const productRouter = require("./product.routes");
 const entregaRouter = require("./entrega.routes");
-const routerUser = require("./user.routes");
-const routerAuth = require("./auth.routes");
+const adminRouter = require("./admin.routes");
+const userRouter = require("./user.routes");
+const authRouter = require("./auth.routes");
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const routes = (app) => {
   app.use(
     "/api",
     express.urlencoded({ extended: false }),
-    routerUser,
+    userRouter,
     productRouter,
-    entregaRouter
+    entregaRouter,
+    adminRouter
   );
-  app.use("/auth", express.urlencoded({ extended: false }), routerAuth);
+  app.use("/auth", express.urlencoded({ extended: false }), authRouter);
 };
 
 module.exports = routes;
