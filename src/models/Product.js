f93812fa5ece362c7feaa1a85045a769c1925b4e
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-06-27 11:41:50 -0300

    Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name:{type: String , required: true },
-    description:{type: String , required: true },
-    value: {type: String , required: true },
-    url_image: {type: String , required: true },
-    createdDate: {type: Date, default: Date.now()}
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  value: { type: String, required: true },
+  url_image: { type: String, required: true },
+  createdDate: { type: Date, default: Date.now() },
 });
 
-const productModel = mongoose.model('product',  productSchema);
+const productModel = mongoose.model("product", productSchema);
 
 module.exports = productModel;
