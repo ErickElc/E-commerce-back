67fd8ee655d14b311618618fc97c69502d6022cc
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2023-06-27 11:41:57 -0300

    Product routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,4 +1,4 @@
-const { authAdmin } = require("../auth/auth");
+const middlewareController = require("../auth/middleware");
 const productController = require("../controllers/productController");
 const express = require("express");
 
@@ -7,6 +7,20 @@ const productRouter = express.Router();
 productRouter
   .get("/products/list/all", productController.listAllProducts)
   .get("/products/list/:id", productController.listOneProduct)
-  .post("/products/new", authAdmin, productController.createProduct);
+  .post(
+    "/products/new",
+    middlewareController.verifyAdmin,
+    productController.createProduct
+  )
+  .put(
+    "/products/update/:id",
+    middlewareController.verifyAdmin,
+    productController.updateProduct
+  )
+  .delete(
+    "/products/delete/:id",
+    middlewareController.verifyAdmin,
+    productController.deleteProduct
+  );
 
 module.exports = productRouter;
